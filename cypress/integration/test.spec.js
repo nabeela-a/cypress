8cf8c632a7ecf11921508781e5df86ab7f18806b
Nabeela Abidi <nabidi@centerfield.com>
2020-03-30 13:24:16 -0700

    added first test

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+describe('AT&T Buyflow', function() {
+  it('visits the ATT App', function() {
+    cy.visit('https://shopatt.centerfieldqa.com/v1/?cookiereset=1')
+   })
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,59 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// required for buyflows
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("saveLocalStorage", () => {
+  Object.keys(localStorage).forEach(key => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add("restoreLocalStorage", () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
+
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   return false;
+// });
+
+// stops test on failure
+// Cypress.on('fail', error => {
+//   Cypress.runner.stop()
+//   throw error; // throw error to have test fail
+// });
+
+beforeEach(() => {
+  cy.fixture('user1.json').as('user')
+  cy.restoreLocalStorage();
+});
+
+afterEach(() => {
+  cy.saveLocalStorage();
+});
